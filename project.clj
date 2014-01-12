13bccac394f06145854b125f16a2b6798ec876f3
Gabriel Horner <gabriel.horner@gmail.com>
2014-01-12 11:02:53 -0500

    convert poc to om

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [cljs-http "0.1.2"]
                  [om "0.1.5"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
--- src/om_freebase_explorer/core.cljs
+++ src/om_freebase_explorer/core.cljs
@@ -4,20 +4,48 @@
               [cljs.core.async :refer [put! <! >! chan timeout]]
               [om.core :as om :include-macros true]
               [om.dom :as dom :include-macros true]
-              [cljs-http.client :as http]
+              [goog.net.Jsonp]
+              [clojure.string :as string]
               [om-freebase-explorer.utils :refer [guid]]))
 
 ;; Lets you do (prn "stuff") to the console
 (enable-console-print!)
 
 (def app-state
-  (atom {:things []}))
+  (atom {}))
+
+(def search-url "https://www.googleapis.com/freebase/v1/search")
+
+(defn jsonp [url params]
+  (let [out (chan 10)]
+    (.send (goog.net.Jsonp. url)
+           params
+           (fn [data] (put! out data))
+           (fn [error] (.log js/console "Jsonp ERROR:" error)))
+    out))
+
+(defn fetch-search-results []
+  (jsonp search-url #js {:query "physics"}))
 
 (defn om-freebase-explorer-app [app owner]
   (reify
+    om/IWillMount
+    (will-mount [_]
+      (go
+       (let [data (<! (fetch-search-results))]
+         (def dt data)
+         (.log js/console "DATA" data)
+         (om/set-state! owner :search-results (.-result data)))))
     om/IRender
-    (render [_]
-      (dom/div nil
-               (dom/h1 nil "om-freebase-explorer is working!")))))
+      (render [_]
+        (dom/div nil
+                 (dom/h1 nil "Welcome to Freebase Explorer!")
+                 (dom/div
+                  #js {:id "search_results"}
+                  (if-let [result (om/get-state owner :search-results)]
+                    (->> (js->clj result :keywordize-keys true)
+                           (map :name)
+                           (string/join ", "))
+                    "No search results"))))))
 
-(om/root app-state om-freebase-explorer-app (.getElementById js/document "content"))
+(om/root app-state om-freebase-explorer-app (.getElementById js/document "app"))
