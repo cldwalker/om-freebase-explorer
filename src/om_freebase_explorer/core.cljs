30a1993944413a0ac2fa61e8b094b0230568840a
Gabriel Horner <gabriel.horner@gmail.com>
2014-01-13 08:00:14 -0500

    freebase search works for more than one call
    Async channel was out of date when scoped as a late.
    Setting it to state, solved the freshness - so obvious, hmmm.

--- src/om_freebase_explorer/core.cljs
+++ src/om_freebase_explorer/core.cljs
@@ -17,56 +17,56 @@
 (def search-url "https://www.googleapis.com/freebase/v1/search")
 
 (defn jsonp [out url params]
-  (let [x :a]
-    (.send (goog.net.Jsonp. url)
+  (.send (goog.net.Jsonp. url)
            params
            (fn [data] (put! out [:search-result data]))
-           (fn [error] (.log js/console "Jsonp ERROR:" error)))
-    out))
+           (fn [error] (.log js/console "Jsonp ERROR:" error))))
 
 (defn fetch-search-results [out query]
   (jsonp out search-url #js {:query query}))
 
+(defn submit-search [chan e]
+  (put! chan [:search
+              (-> (.querySelector js/document "#search_term")
+                  .-value)])
+  false)
+
 (defn search-form [app owner {:keys [chan]}]
   (om/component
-   (dom/div nil
-            (dom/input
-             #js {:id "search_term"})
-            (dom/button
-             #js {:onClick
-                  (fn [e]
-                    (put! chan [:search
-                                (-> (.querySelector js/document "#search_term")
-                                    .-value)]))}
-             "Search"))))
+   (dom/form #js {:onSubmit #(submit-search chan %) }
+            (dom/input #js {:type "text" :id "search_term"})
+            (dom/input #js {:type "submit" :value "Search"}))))
+
+(defn handle-event [event event-data {:keys [chan owner]}]
+  (.log js/console "Event: " event event-data)
+  (case event
+    :search (fetch-search-results chan event-data)
+    :search-result (om/set-state! owner :search-results (.-result event-data))
+    (.log js/console "No event found for" event event-data)))
 
 (defn om-freebase-explorer-app [app owner]
-  (let [search-ch (chan)]
-    (reify
+  (reify
       om/IWillMount
       (will-mount [_]
-                  (go (while true
-                        (.log js/console "Loop...")
-                        (let [[event event-data](<! search-ch)]
-                          (.log js/console "Event: " event event-data)
-                          (case
-                            event
-                            :search (fetch-search-results search-ch event-data)
-                            :search-result (om/set-state! owner :search-results (.-result event-data))
-                            (.log js/console "No event found for" event event-data))
-                          )))
-                  )
+                  (let [main-chan (chan 10)]
+                    (om/set-state! owner :chan main-chan)
+                    (go (while true
+                          (let [[event event-data](<! main-chan)]
+                            (handle-event
+                             event
+                             event-data
+                             {:chan main-chan :owner owner}))))))
       om/IRender
       (render [_]
               (dom/div nil
                        (dom/h1 nil "Welcome to Freebase Explorer!")
-                       (om/build search-form app {:opts {:chan search-ch}})
+                       (om/build search-form app {:opts {:chan (om/get-state owner :chan)}})
                        (dom/div
                         #js {:id "search_results"}
                         (if-let [result (om/get-state owner :search-results)]
                           (->> (js->clj result :keywordize-keys true)
                                (map :name)
                                (string/join ", "))
-                          "No search results")))))))
+                          "No search results"))))))
 
 (om/root app-state om-freebase-explorer-app (.getElementById js/document "app"))
